56c462e20ece3b7a880d7f991ed1703d46938828
Natenael <nattymultamirat@gmail.com>
2024-02-04 20:55:22 +0300

    Update TodayWeather.js

--- src/components/TodayWeather.js
+++ src/components/TodayWeather.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import WeatherStatus from './DayBlock.js'; //check here // Assuming 'StatusHoje' means 'DayBlock' in Portuguese
-import InfoBlock from "./InfoBlock.js"; //check here // Assuming 'BlocoInformacoes' means 'InformationBlock' in Portuguese
-import TextBar from '../components/TextBar.js'; // Assuming 'BarraTexto' means 'TextBar' in Portuguese
+import TodayStatus from './DayBlock.js'; //check here 
+import InfoBlock from "./InfoBlock.js"; //check here 
+import TextBar from '../components/TextBar.js'; 
 
 export default function TodayWeather(props) {
     return (
         <>
             <View style={styles.container}>
-                <WeatherStatus type={props.activeCard} />
+                <TodayStatus type={props.activeCard} />
                 <InfoBlock type={props.activeCard} />
             </View>
             <TextBar />
