48d916f95583c4c017b6ae103332a4e6dfdba9dc
Natenael <nattymultamirat@gmail.com>
2024-02-04 21:50:29 +0300

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import 'react-native-gesture-handler';
 import { StyleSheet, StatusBar, SafeAreaView ,Platform,View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import TelaProximosDias from './src/telas/TelaProximosDias.js';
+import NextDaysScreen from './src/screens/NextDaysScreen.js';
 import TelaTempoHoje from './src/telas/TelaTempoHoje.js';
 import DrawerContent from './src/componentes/DrawerContent';
 
@@ -36,7 +36,7 @@ export default function App() {
       <NavigationContainer>
       <Stack.Navigator  screenOptions={{headerShown: false}}>
       <Stack.Screen name="Home" component={Home}/>
-      <Stack.Screen name="Next 7 days" component={TelaProximosDias}/>
+      <Stack.Screen name="Next 7 days" component={NextDaysScreen}/>
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaView>
